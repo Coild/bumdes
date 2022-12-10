565454df6abb750dbc8dbae7fa037d10264b67aa
Coild <xreza75@gmail.com>
2022-12-10 10:39:17 +0800

    datatable menu penglela

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('fitur.dashboard');
 });
+
+Route::get('/pengelola', function () {
+    return view('fitur.pengelola');
+});
