d100b3179388fd5987fe8e9d25a83298c42c4f66
Coild <xreza75@gmail.com>
2022-12-21 10:48:16 +0800

    tambah css datepicker

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -16,6 +16,13 @@
     <link href={{ asset('plugins/datatables/responsive.bootstrap.min.css') }} rel="stylesheet" type="text/css" />
     <link href={{ asset('plugins/datatables/dataTables.bootstrap.min.css') }} rel="stylesheet" type="text/css" />
 
+    {{-- date picker --}}
+    <link href={{ asset("plugins/timepicker/bootstrap-timepicker.min.css") }} rel="stylesheet">
+    <link href={{ asset("plugins/bootstrap-colorpicker/css/bootstrap-colorpicker.min.css") }} rel="stylesheet">
+    <link href={{ asset("plugins/bootstrap-datepicker/css/bootstrap-datepicker.min.css") }} rel="stylesheet">
+    <link href={{ asset("plugins/bootstrap-touchspin/css/jquery.bootstrap-touchspin.min.css") }} rel="stylesheet" />
+
+
 
     <link href={{ asset('css/bootstrap.min.css') }} rel="stylesheet" type="text/css">
     <link href={{ asset('css/icons.css') }} rel="stylesheet" type="text/css">
