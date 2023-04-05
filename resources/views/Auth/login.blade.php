ea2990bc3088fef2a941b59a69f8cd2a5463a977
Coild <xreza75@gmail.com>
2023-04-05 13:33:00 +0800

    coba bentrok

--- resources/views/Auth/login.blade.php
+++ resources/views/Auth/login.blade.php
@@ -49,7 +49,7 @@
                                 <div>
                                     {{-- <input id="checkbox-signup" type="checkbox"> --}}
                                     <button class="btn-link" onclick="window.location.href='lupa'">
-                                        Lupa Kata Sandi
+                                        Lupa Kata Sandi Bukan Cici
                                     </button>
                                 </div>
                             </div>
